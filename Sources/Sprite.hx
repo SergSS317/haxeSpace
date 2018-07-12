1e4d6d293e7fdc060db41ca96fdb62000818948b
SergSS317 <40733976+SergSS317@users.noreply.github.com>
2018-07-12 23:28:19 +0300

    to 3d
    This reverts commit d346ac33a52eecb64292b888c64398dac66770cb.

--- Sources/Sprite.hx
+++ Sources/Sprite.hx
@@ -3,23 +3,31 @@ import kha.Assets;
 import kha.Image;
 import kha.graphics4.Graphics;
 import kha.math.Vector3;
+<<<<<<< HEAD
 import kha.Color;
 import kha.graphics4.TextureUnit;
+=======
+
+>>>>>>> parent of d346ac3... to 3d
 /**
  * ...
  * @author ...
  */
- //import Entity;
-class Sprite extends Entity 
+class Sprite 
 {
 
+<<<<<<< HEAD
 	
 	//public var position:Vector3 = new Vector3(0, 0, 0);
+=======
+	public var position:Vector3 = new Vector3(0, 0, 0);
+>>>>>>> parent of d346ac3... to 3d
 	public var img:Image;
-	public override function new( _image:Image, _position:Vector3, _size:Vector3) 
+	public function new(_position:Vector3, _image:Image) 
 	{
-		this.Position = _position;
+		this.position = _position;
 		this.img = _image;
+<<<<<<< HEAD
 		this.color = Color.White;
 		//this.Size = _size;
 		//Base.AllSprites.push(this);
@@ -41,7 +49,9 @@ class Sprite extends Entity
 	{
 		super.update();
 	}*/
+=======
+		
+	}
+>>>>>>> parent of d346ac3... to 3d
 	
-	
-
 }
