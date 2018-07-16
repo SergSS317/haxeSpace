e304634a8f9b43b901f71f54672dc785ba7dd404
SergSS317 <40733976+SergSS317@users.noreply.github.com>
2018-07-16 20:49:34 +0300

    Revert "Revert "Revert "mb experiment"""
    This reverts commit 2bca059ff2a81379d22b5c1bcd07c0f105b15248.

--- Sources/Drawning.hx
+++ Sources/Drawning.hx
@@ -1,6 +1,5 @@
 package;
 //import haxe.io.Float32Array;
-import kha.FastFloat;
 import kha.graphics1.Graphics4;
 import kha.graphics4.Graphics;
 import kha.math.Vector2;
@@ -36,7 +35,7 @@ class Drawning
 {
 	var vertexBuffer:VertexBuffer;
 	var indexBuffer:IndexBuffer;
-	
+
 	//var mvp:FastMatrix4;
 	
 
@@ -51,14 +50,6 @@ class Drawning
 	public function new(_imageData:Image) 
 	{
 		imageData = _imageData;
-		// Create vertex buffer
-		vertexBuffer = new VertexBuffer( 
-			Std.int(480000), // Vertex count - 3 floats per vertex
-			Set3d.structure, // Vertex structure
-			Usage.DynamicUsage // Vertex data will stay the same
-		);
-
-		CreateIndexBuffer(480000);
 		//loadingFinished(_imageData:Image);
 	}
 	
@@ -170,9 +161,8 @@ if(a < 100){
 		//trace("Base.AllSprites:"+Base.AllSprites.length+"   vertices:"+vertices.length);
 		//trace("Drawning end update");
 	}
-	public static var UdateUV:Bool = false;
-	public static var UdateColor:Bool = false;
-	public static var UdateVertex:Bool=false;
+	//public static var UdateIndex:Bool = false;
+	//public static var UdateVertex:Bool=false;
 	function PreRender()
 	{
 		//trace("Base.AllSprites:"+vertices.length);
@@ -181,12 +171,11 @@ if(a < 100){
 		textureID = Set3d.pipeline.getTextureUnit("Light9");
 		// Texture
 		
-		 CreateVertexBuffer(UdateVertex,UdateUV,UdateColor);
+		 CreateVertexBuffer();
 		//if(UdateVertex) CreateVertexBuffer();
 		//UdateVertex = false;
 		//if (UdateIndex) 
-		
-		//CreateIndexBuffer();
+		CreateIndexBuffer();
 		//UdateIndex = false;
 	}
 	
@@ -194,63 +183,18 @@ if(a < 100){
 	
 
 		
-	public function CreateVertexBuffer(updVert:Bool,upduv:Bool,updcolor:Bool)
+	public function CreateVertexBuffer()
 		{
 			//---------------------------------------------------------
-			/*	vertexBuffer = new VertexBuffer(
+		// Create vertex buffer
+		vertexBuffer = new VertexBuffer(
 			Std.int(vertices.length / 3), // Vertex count - 3 floats per vertex
 			Set3d.structure, // Vertex structure
-			Usage.DynamicUsage // Vertex data will stay the same
-		);*/
+			Usage.StaticUsage // Vertex data will stay the same
+		);
 		//---------------------------------------------------------
 		//GenvbData();
 		var vbData = vertexBuffer.lock();
-		for (i in 0...Std.int(vbData.length / Set3d.structureLength)) {
-			//vbData.set(i * structureLength, vertices[i * 3]+Position.x);
-			//vbData.set(i * structureLength + 1, vertices[i * 3 + 1]+Position.y);
-			if (updVert)
-			{
-			if (vertices[i * 3] != null)
-			{
-				vbData.set(i * Set3d.structureLength, vertices[i * 3]);
-				vbData.set(i * Set3d.structureLength + 1, vertices[i * 3 + 1]);
-				vbData.set(i * Set3d.structureLength + 2, vertices[i * 3 + 2]);
-			}else{
-				vbData.set(i * Set3d.structureLength, 0);
-				vbData.set(i * Set3d.structureLength + 1, 0);
-				vbData.set(i * Set3d.structureLength + 2, 0);
-			}
-			}
-			if(upduv){
-			if(uvs[i * 2]!=null){
-				vbData.set(i * Set3d.structureLength + 3, uvs[i * 2]);
-				vbData.set(i * Set3d.structureLength + 4, uvs[i * 2 + 1]);
-			}else{
-				vbData.set(i * Set3d.structureLength + 3, 0);
-				vbData.set(i * Set3d.structureLength + 4, 0);
-			}
-			}
-			if(updcolor){
-			if (colors[i * 4] != null){
-				vbData.set(i * Set3d.structureLength + 5, colors[i * 4]);
-				vbData.set(i * Set3d.structureLength + 6, colors[i * 4 + 1]);
-				vbData.set(i * Set3d.structureLength + 7, colors[i * 4 + 2]);
-				vbData.set(i * Set3d.structureLength + 8, colors[i * 4 + 3]);
-			}else{
-				vbData.set(i * Set3d.structureLength + 5, 0);
-				vbData.set(i * Set3d.structureLength + 6, 0);
-				vbData.set(i * Set3d.structureLength + 7, 0);
-				vbData.set(i * Set3d.structureLength + 8, 0);
-			}
-			}
-
-			//vbData.set(i * Set3d.structureLength + 9, Camera.zoom);
-		}
-		vertexBuffer.unlock();
-		UdateUV = false;
-		UdateColor = false;
-		UdateVertex = false;
-		/*var vbData = vertexBuffer.lock();
 		for (i in 0...Std.int(vbData.length / Set3d.structureLength)) {
 			//vbData.set(i * structureLength, vertices[i * 3]+Position.x);
 			//vbData.set(i * structureLength + 1, vertices[i * 3 + 1]+Position.y);
@@ -265,39 +209,16 @@ if(a < 100){
 			vbData.set(i * Set3d.structureLength + 8, colors[i * 4 + 3]);
 			//vbData.set(i * Set3d.structureLength + 9, Camera.zoom);
 		}
-		vertexBuffer.unlock();*/
+		vertexBuffer.unlock();
 		}
 		
-				function CreateIndexBuffer(count:Int){
+		function CreateIndexBuffer(){
 		// A 'trick' to create indices for a non-indexed vertex data
-		//trace("indices:"+vertices.length);
-		var indices:Array<Int> = [];
-		for (i in 0...Std.int(count)) {
-			indices.push(i);
-		}
-//trace("indic: "+indices.length);
-		// Create index buffer
-		indexBuffer = new IndexBuffer(
-			indices.length, // Number of indices for our cube
-			Usage.StaticUsage // Index data will stay the same
-		);
-		
-		// Copy indices to index buffer
-		var iData = indexBuffer.lock();
-		for (i in 0...iData.length) {
-			iData[i] = indices[i];
-		}
-		indexBuffer.unlock();
-		}
-		
-		/*function CreateIndexBuffer(){
-		// A 'trick' to create indices for a non-indexed vertex data
-		//trace("indices:"+vertices.length);
 		var indices:Array<Int> = [];
 		for (i in 0...Std.int(vertices.length / 3)) {
 			indices.push(i);
 		}
-//trace("indic: "+indices.length);
+
 		// Create index buffer
 		indexBuffer = new IndexBuffer(
 			indices.length, // Number of indices for our cube
@@ -310,7 +231,7 @@ if(a < 100){
 			iData[i] = indices[i];
 		}
 		indexBuffer.unlock();
-		}*/
+		}
 		
 	public function render(g:Graphics) {
 //		var temp:Float = Scheduler.time();
